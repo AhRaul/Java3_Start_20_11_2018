9ad4dd47df988bf6a4620a087c99552744919b36
gg <gartanolio@yandex.ru>
2018-11-22 00:24:41 +0300

    Create project HW1 J3 t2

---
+++ HW1_Raul_JavaProfi/src/ru/geekbrains/git/Fruit.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.git;
+
+/**
+ * родительский класс "Фрукты"
+ */
+public class Fruit {
+    public String type;
+    public float weight;
+}
---
+++ HW1_Raul_JavaProfi/src/ru/geekbrains/git/Orange.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.git;
+
+/**
+ * класс "Апельсины"
+ */
+public class Orange extends Fruit {
+    public static final float ORANGE_WEIGHT = 1.5f;
+
+    public Orange(){
+        super.type = "Orange";
+        super.weight = ORANGE_WEIGHT;
+    }
+
+}
